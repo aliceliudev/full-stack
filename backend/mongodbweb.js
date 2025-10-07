043e39e04428cf29d1bf0608a9120ad13b612db7
aliceliudev <80049730+aliceliudev@users.noreply.github.com>
2025-10-07 14:37:09 +0000

    chore: w2 initial react vite project

---
+++ backend/mongodbweb.js
@@ -0,0 +1,32 @@
+import { createServer } from "node:http";
+import { MongoClient } from "mongodb";
+
+const url = "mongodb://localhost:27017";
+const dbName = "mydb";
+const client = new MongoClient(url);
+
+(async () => {
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+  } catch (err) {
+    console.error("Error connecting to MongoDB", err);
+  }
+})();
+
+const server = createServer(async (req, res) => {
+  const db = client.db(dbName);
+  const users = db.collection("users");
+  const userList = await users.find({}).toArray();
+
+  res.statusCode = 200;
+
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(userList));
+});
+
+const host = "localhost";
+const port = 3000;
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
